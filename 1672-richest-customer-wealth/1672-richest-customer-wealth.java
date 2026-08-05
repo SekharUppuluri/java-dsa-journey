b58798d6b29852e16a3e6dac483d90d71e7bf97d
Sekhar <sekharuppuluri3@gmail.com>
2026-08-05 11:02:23 +0530

    Clean up code and remove comments
    Removed unnecessary comments and synced code.

--- 1672-richest-customer-wealth/1672-richest-customer-wealth.java
+++ 1672-richest-customer-wealth/1672-richest-customer-wealth.java
@@ -2,14 +2,17 @@ class Solution {
     public int maximumWealth(int[][] accounts) {
 
         int personWealth = Integer.MIN_VALUE;
-        
+
+        // going through every person data
         for(int person = 0 ; person < accounts.length; person++ ){
-            
+
+            // sum for person bank accounts (col)
             int personSum = 0;
             for(int bank = 0 ; bank < accounts[person].length; bank++ ) {
                 personSum += accounts[person][bank];
             }
 
+            // comparing highest number of bank accounts of a person
             if (personSum > personWealth){
                 personWealth = personSum;
             }
@@ -17,7 +20,3 @@ class Solution {
         return personWealth;
     }
 }
-
-// Synced seamlessly with LeetHub Pro
-// Pro features: https://bit.ly/leethubpro | Free version: https://bit.ly/leethubv4
-// Get it here: https://chromewebstore.google.com/detail/bcilpkkbokcopmabingnndookdogmbna
