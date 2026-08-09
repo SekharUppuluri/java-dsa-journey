a41a90884ec68bcdb3184578a6949f3ce007564e
Sekhar <sekharuppuluri3@gmail.com>
2026-08-09 20:19:24 +0530

    Time: 44 ms (98.94%), Space: 146.1 MB (24.13%) - LeetHub - Time: O(n log n + m log m), Space: O(1)

---
+++ 4014-minimum-total-price-after-applying-discounts/4014-minimum-total-price-after-applying-discounts.java
@@ -0,0 +1,26 @@
+class Solution {
+    public double minPrice(int[] prices, int[] discounts) {
+
+        Arrays.sort(prices);
+        Arrays.sort(discounts);
+        
+        double total_price = 0;
+        int price_end = prices.length - 1 , discounts_end = discounts.length - 1;
+        int i = 0;
+
+        while ( price_end - i >= 0 && discounts_end - i >= 0){
+            double discounted_price = (prices[price_end - i] * (100 - discounts[discounts_end - i]))/ (double) 100 ;
+            total_price += discounted_price;
+            i++;
+        }
+
+        for(int j = 0 ; j < prices.length - i ; j++){
+            total_price += prices[j];
+        }
+        return total_price;
+    }
+}
+
+// Synced seamlessly with LeetHub Pro
+// Pro features: https://bit.ly/leethubpro | Free version: https://bit.ly/leethubv4
+// Get it here: https://chromewebstore.google.com/detail/bcilpkkbokcopmabingnndookdogmbna
